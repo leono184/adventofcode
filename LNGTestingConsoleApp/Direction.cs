7fdd8a197c6f0305af863e13d6c74586f3777f1a
Leo Nordling <leo.nordling@insyde.se>
2023-12-23 15:10:21 +0100

    Add project files.

---
+++ LNGTestingConsoleApp/Direction.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LNGTestingConsoleApp;
+public record Direction(string key, string Left, string Right)
+{
+    public static Direction New(string inputLine)
+    {
+        return new(inputLine.Substring(0,3),GetStringBetweenCharacters(inputLine, "(", ","),
+        GetStringBetweenCharacters(inputLine, ", ", ")"));
+    }
+
+    public static string GetStringBetweenCharacters(string input, string charFrom, string charTo) {
+        int posFrom = input.IndexOf(charFrom);
+        if(posFrom != -1) //if found char
+        {
+            int posTo = input.IndexOf(charTo, posFrom + 1);
+            if(posTo != -1) //if found char
+            {
+                return input.Substring(posFrom + charFrom.Length, posTo - posFrom - charFrom.Length);
+            }
+        }
+
+        return string.Empty;
+    }
+
+    public string GetNewPoint(char direction) {
+        if(direction == 'l' || direction =='L') {
+            return Left;
+        }
+        else if(direction == 'r' || direction == 'R') {
+            return Right;
+        }
+        else
+        {
+            throw new Exception("wrong input");
+        }
+    }
+}
+
---
+++ LNGTestingConsoleApp/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Read a text file line by line.
+using LNGTestingConsoleApp;
+const string START = "AAA";
+const string END = "ZZZ";
+
+int steps = 0;
+char nextStep;
+string tempSequence;
+string sequence;
+var directions = new Dictionary<string, Direction>();
+
+string[] lines = File.ReadAllLines("C:\\Users\\leono\\Downloads\\AoC 2023 Day 8 input.txt");
+
+sequence = lines[0];
+
+foreach(string line in lines.Skip(2)) {
+    directions.Add(line.Substring(0, 3),
+                   Direction.New(line));
+}
+
+var startPosition = directions[START];
+var endPosition = directions[END];
+var currentPosition = startPosition;
+
+tempSequence = sequence;
+
+while(currentPosition != endPosition){
+
+    steps++;
+    nextStep = tempSequence[0];
+
+    tempSequence = tempSequence.Length == 1 ? sequence : tempSequence.Substring(1);
+
+    currentPosition = directions[currentPosition.GetNewPoint(nextStep)];
+}
+
+Console.WriteLine(steps);
